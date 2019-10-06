52260471a46f51028d77d48762b5715f9287fa64
Jeonata <jeonata@gmail.com>
2019-10-06 16:19:23 -0300

    Chore: Add resources - Create all screen, component and _layouts folders - Add services (api and history) - Add reactotron - Add global styles - Add routes - Styles: Stylize 'SignIn' an 'SignUp'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
+
+import GlobalStyles from './styles/global';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyles />
+      <ToastContainer autoClose={3000} />
+    </Router>
+  );
 }
 
 export default App;
